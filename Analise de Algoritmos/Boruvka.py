5f03918ccda6915888451fcf7a5795d51b105c50
LeoMedeirosss <lmf@cesar.school>
2024-10-31 14:07:16 -0300

    Boruvka - código comentado

--- Analise de Algoritmos/Boruvka.py
+++ Analise de Algoritmos/Boruvka.py
@@ -1,112 +1,108 @@
 from collections import defaultdict
  
-#Class to represent a graph
+# Classe para representar um grafo
 class Graph:
  
-    def __init__(self,vertices):
-        self.V= vertices #No. of vertices
-        self.graph = [] # default dictionary to store graph
+    def __init__(self, vertices):
+        self.V = vertices  # Número de vértices
+        self.graph = []  # dicionário padrão para armazenar o grafo
          
-  
-    # function to add an edge to graph
-    def addEdge(self,u,v,w):
-        self.graph.append([u,v,w])
+    # Função para adicionar uma aresta ao grafo
+    def addEdge(self, u, v, w):
+        self.graph.append([u, v, w])
  
-    # A utility function to find set of an element i
-    # (uses path compression technique)
+    # Função utilitária para encontrar o conjunto de um elemento i
+    # (usa a técnica de compressão de caminho)
     def find(self, parent, i):
         if parent[i] == i:
             return i
         return self.find(parent, parent[i])
  
-    # A function that does union of two sets of x and y
-    # (uses union by rank)
+    # Função para unir dois conjuntos x e y
+    # (usa união por rank)
     def union(self, parent, rank, x, y):
         xroot = self.find(parent, x)
         yroot = self.find(parent, y)
  
-        # Attach smaller rank tree under root of high rank tree
-        # (Union by Rank)
+        # Anexa a árvore de menor rank na raiz da árvore de maior rank
+        # (União por Rank)
         if rank[xroot] < rank[yroot]:
             parent[xroot] = yroot
         elif rank[xroot] > rank[yroot]:
             parent[yroot] = xroot
-        #If ranks are same, then make one as root and increment
-        # its rank by one
-        else :
+        # Se os ranks são iguais, então faz um deles como raiz e incrementa
+        # seu rank em um
+        else:
             parent[yroot] = xroot
             rank[xroot] += 1
  
-    # The main function to construct MST using Kruskal's algorithm
+    # Função principal para construir a MST usando o algoritmo de Borůvka
     def boruvkaMST(self):
-        parent = []; rank = []; 
+        parent = []
+        rank = []
  
-        # An array to store index of the cheapest edge of
-        # subset. It store [u,v,w] for each component
-        cheapest =[]
+        # Um array para armazenar o índice da aresta mais barata de cada
+        # subconjunto. Ele armazena [u, v, w] para cada componente
+        cheapest = []
  
-        # Initially there are V different trees.
-        # Finally there will be one tree that will be MST
+        # Inicialmente, existem V árvores diferentes.
+        # Finalmente, haverá uma árvore única, que será a MST
         numTrees = self.V
         MSTweight = 0
  
-        # Create V subsets with single elements
+        # Cria V subconjuntos com elementos únicos
         for node in range(self.V):
             parent.append(node)
             rank.append(0)
-            cheapest =[-1] * self.V
+            cheapest = [-1] * self.V
      
-        # Keep combining components (or sets) until all
-        # components are not combined into single MST
- 
+        # Continua combinando componentes (ou conjuntos) até que todos
+        # os componentes sejam combinados em uma única MST
         while numTrees > 1:
  
-            # Traverse through all edges and update
-               # cheapest of every component
+            # Percorre todas as arestas e atualiza
+            # a aresta mais barata de cada componente
             for i in range(len(self.graph)):
  
-                # Find components (or sets) of two corners
-                # of current edge
-                u,v,w =  self.graph[i]
+                # Encontra os componentes (ou conjuntos) das duas extremidades
+                # da aresta atual
+                u, v, w = self.graph[i]
                 set1 = self.find(parent, u)
-                set2 = self.find(parent ,v)
+                set2 = self.find(parent, v)
  
-                # If two corners of current edge belong to
-                # same set, ignore current edge. Else check if 
-                # current edge is closer to previous
-                # cheapest edges of set1 and set2
+                # Se as duas extremidades da aresta atual pertencem ao
+                # mesmo conjunto, ignora essa aresta. Caso contrário,
+                # verifica se a aresta atual é a mais próxima
+                # das arestas mais baratas dos conjuntos set1 e set2
                 if set1 != set2:     
-                     
-                    if cheapest[set1] == -1 or cheapest[set1][2] > w :
-                        cheapest[set1] = [u,v,w] 
+                    if cheapest[set1] == -1 or cheapest[set1][2] > w:
+                        cheapest[set1] = [u, v, w] 
+                    if cheapest[set2] == -1 or cheapest[set2][2] > w:
+                        cheapest[set2] = [u, v, w]
  
-                    if cheapest[set2] == -1 or cheapest[set2][2] > w :
-                        cheapest[set2] = [u,v,w]
- 
-            # Consider the above picked cheapest edges and add them
-            # to MST
+            # Considera as arestas mais baratas selecionadas e as adiciona
+            # à MST
             for node in range(self.V):
  
-                #Check if cheapest for current set exists
+                # Verifica se a aresta mais barata para o conjunto atual existe
                 if cheapest[node] != -1:
-                    u,v,w = cheapest[node]
+                    u, v, w = cheapest[node]
                     set1 = self.find(parent, u)
-                    set2 = self.find(parent ,v)
+                    set2 = self.find(parent, v)
  
-                    if set1 != set2 :
+                    if set1 != set2:
                         MSTweight += w
                         self.union(parent, rank, set1, set2)
-                        print ("Edge %d-%d with weight %d included in MST" % (u,v,w))
-                        numTrees = numTrees - 1
+                        print("Aresta %d-%d com peso %d incluída na MST" % (u, v, w))
+                        numTrees -= 1
              
-            #reset cheapest array
-            cheapest =[-1] * self.V
+            # Reseta o array de arestas mais baratas
+            cheapest = [-1] * self.V
  
-             
-        print ("Weight of MST is %d" % MSTweight)
+        print("Peso da MST é %d" % MSTweight)
                            
  
-     
+# Exemplo de uso
 g = Graph(4)
 g.addEdge(0, 1, 10)
 g.addEdge(0, 2, 6)
